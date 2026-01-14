1a19d84ee92ea19e55a6c428d4b555762cff5f93
Diana <dianaibarra0303@gmail.com>
2026-01-14 13:55:24 -0600

    se modificaron los archivos

---
+++ script.js
@@ -0,0 +1,6 @@
+const h3Element = document.getElementById("Adios");
+const h1Element = document.getElementById("orange");
+const imgElement = document.getElementById("size")
+
+console.log("Adios");
+h1Element.style.color = "orange"
